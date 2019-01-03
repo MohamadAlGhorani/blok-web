edbee0d516ad20a8c3fd99e28fd79631722c7b87
MohamadAlGhorani <moh.gh266@gmail.com>
2019-01-03 21:43:52 +0100

    initial commit

--- Js/index.js
+++ Js/index.js
@@ -1,2 +1,41 @@
-var score = 0;
-var genreElement= document.getElementById('genre');
+var genre = document.querySelector('header form:last-of-type input:first-of-type');
+var form = document.querySelector('header form:last-of-type');
+var filter = document.querySelector('header nav ul li button');
+var formFilter = document.querySelector('header form:nth-of-type(2)');
+var search = document.querySelector('header nav li:last-of-type button');
+var formSearch = document.querySelector('header form:first-of-type');
+var like = document.querySelector('article li:first-of-type button');
+
+var slider = document.querySelector("header input[type=range]");
+var output = document.querySelector("header form:nth-of-type(2) fieldset:nth-of-type(2) p span");
+output.innerHTML = slider.value;
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
+
+
+function formSubmit() {
+    form.submit();
+}
+
+function filteren() {
+    formFilter.classList.toggle('filteren');
+}
+
+function zoeken() {
+    formSearch.classList.toggle('zoeken');
+}
+
+function likeFunctie() {
+    like.classList.toggle('ani');
+}
+
+function terug() {
+    filter.classList.toggle('che');
+}
+
+genre.addEventListener('click', formSubmit);
+filter.addEventListener('click', filteren);
+search.addEventListener('click', zoeken);
+like.addEventListener('click', likeFunctie);
+filter.addEventListener('click', terug);
