fc88a164c56bdb261788b5fcc6be965088548481
MohamadAlGhorani <moh.gh266@gmail.com>
2019-01-07 11:47:17 +0100

    initial commit

--- Js/index.js
+++ Js/index.js
@@ -5,7 +5,11 @@ var formFilter = document.querySelector('header form:nth-of-type(2)');
 var search = document.querySelector('header nav li:last-of-type button');
 var formSearch = document.querySelector('header form:first-of-type');
 var like = document.querySelector('article li:first-of-type button');
+var favorit = document.querySelector('section article header button');
 var verrasMe = document.querySelector('header form:last-of-type input:nth-of-type(4)');
+var likeElement = document.querySelector('body section article ul span li:last-of-type');
+var likeGetal = 50;
+var status = 'een';
 
 var slider = document.querySelector("header input[type=range]");
 var output = document.querySelector("header form:nth-of-type(2) fieldset:nth-of-type(2) p span");
@@ -14,14 +18,26 @@ slider.oninput = function () {
     output.innerHTML = this.value;
 }
 
+function likeNummer() {
+
+    if (status == 'een') {
+        likeGetal = likeGetal + 1;
+        likeElement.textContent = likeGetal;
+        status = 'twee'
+    } else if (status == 'twee') {
+        likeGetal = likeGetal - 1;
+        likeElement.textContent = likeGetal;
+        status = 'een';
+    }
+}
 
 function formSubmit() {
-    form.action = ("blok-web/genre.html");
+    form.action = "/genre.html";
     form.submit();
 }
 
 function formSubmitTwee() {
-    form.action = ("blok-web/verrasme.html");
+    form.action = "/verrasme.html";
     form.submit();
 }
 
@@ -37,6 +53,10 @@ function likeFunctie() {
     like.classList.toggle('ani');
 }
 
+function favorietFunctie() {
+    favorit.classList.toggle('ani');
+}
+
 function terug() {
     filter.classList.toggle('che');
 }
@@ -46,4 +66,7 @@ verrasMe.addEventListener('click', formSubmitTwee);
 filter.addEventListener('click', filteren);
 search.addEventListener('click', zoeken);
 like.addEventListener('click', likeFunctie);
+favorit.addEventListener('click', favorietFunctie);
 filter.addEventListener('click', terug);
+like.addEventListener('click', likeNummer);
+
