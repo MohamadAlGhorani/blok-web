5f32cf3e9fd297158dac1b0ee1682e726fac161b
MohamadAlGhorani <moh.gh266@gmail.com>
2019-01-08 15:16:22 +0100

    initial commit

--- Js/index.js
+++ Js/index.js
@@ -5,13 +5,14 @@ var formFilter = document.querySelector('header form:nth-of-type(2)');
 var search = document.querySelector('header nav li:last-of-type button');
 var formSearch = document.querySelector('header form:first-of-type');
 var like = document.querySelectorAll('article li:first-of-type button');
-console.log("like",like);
-var favorit = document.querySelector('section article header button');
+console.log("like", like);
+var favorit = document.querySelectorAll('section article header button');
+console.log("favorit", favorit);
 var verrasMe = document.querySelector('header form:last-of-type input:nth-of-type(4)');
 var likeElement = document.querySelector('body section article ul span li:last-of-type');
-var likeGetal = 50;
+var likeGetal = parseInt(likeElement.innerText);
+console.log(likeGetal + 1);
 var status = 'een';
-
 var slider = document.querySelector("header input[type=range]");
 var output = document.querySelector("header form:nth-of-type(2) fieldset:nth-of-type(2) p span");
 output.innerHTML = slider.value;
@@ -20,11 +21,10 @@ slider.oninput = function () {
 }
 
 function likeNummer() {
-
     if (status == 'een') {
         likeGetal = likeGetal + 1;
         likeElement.textContent = likeGetal;
-        status = 'twee'
+        status = 'twee';
     } else if (status == 'twee') {
         likeGetal = likeGetal - 1;
         likeElement.textContent = likeGetal;
@@ -50,14 +50,6 @@ function zoeken() {
     formSearch.classList.toggle('zoeken');
 }
 
-function likeFunctie() {
-    like.classList.toggle('ani');
-}
-
-function favorietFunctie() {
-    favorit.classList.toggle('ani');
-}
-
 function terug() {
     filter.classList.toggle('che');
 }
@@ -67,20 +59,22 @@ verrasMe.addEventListener('click', formSubmitTwee);
 filter.addEventListener('click', filteren);
 search.addEventListener('click', zoeken);
 
-for(var i=0;i<like.length; i++){
-    like[i].addEventListener("click",function(){
-  console.log(this);
-    this.classList.toggle('ani');
-});
-}//end for
+for (var i = 0; i < like.length; i++) {
+    like[i].addEventListener("click", function () {
+        console.log(this);
+        this.classList.toggle('ani');
+        likeNummer(this);
+    });
+} //end for
 
+for (var i = 0; i < favorit.length; i++) {
+    favorit[i].addEventListener("click", function () {
+        console.log(this);
+        this.classList.toggle('ani');
 
+    });
+} //end for
 
-
-//like[0].addEventListener('click', likeFunctie);
-
-favorit.addEventListener('click', favorietFunctie);
 filter.addEventListener('click', terug);
 
 //like.addEventListener('click', likeNummer);
-
